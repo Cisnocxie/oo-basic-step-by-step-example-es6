1c38097260c660c11cf0a0832375cc918e2b2f8a
Cisnoc Xie <502021156@qq.com>
2018-07-10 01:15:05 +0800

    pass p7

--- src/practice_7/teacher.js
+++ src/practice_7/teacher.js
@@ -1,1 +1,17 @@
+var Person = require("../../src/practice_7/person.js");
+class Teacher extends Person{
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 
+    introduce() {
+        return super.introduce() + (!this.klass ? ' I am a Teacher. I teach No Class.' : ' I am a Teacher. I teach Class ' + this.klass.number + '.');
+    }
+
+    introduceWith(student) {
+        return super.introduce() + ' I am a Teacher. I ' + ((this.klass && this.klass.number === student.klass.number) ? '' : "don't ") + 'teach ' + student.name + '.';
+    }
+}
+
+module.exports = Teacher;
