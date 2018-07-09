071305d3f630f87124fff8e3e39647261ead3c9c
Cisnoc Xie <502021156@qq.com>
2018-07-10 01:16:17 +0800

    pass p8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,21 @@
+class Class {
+    constructor(klass) {
+        this.number = klass;
+    }
 
+    getDisplayName() {
+        return 'Class ' + this.number;
+    }
+    
+    assignLeader(leader) {
+        if (this.equal(leader.klass)) {
+            this.leader = leader;
+        }
+    }
+    
+    equal(klass) {
+        return this.number === klass.number;
+    }
+}
+
+module.exports = Class;
